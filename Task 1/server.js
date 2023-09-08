4d4081dc558509a3b88f43b1daaa173f22fe38ff
Bakarray <magnificentzeem@gmail.com>
2023-09-08 15:12:41 +0100

    first commit

---
+++ Task 1/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
+
+app.get('/api', (req, res) => {
+    const slackName = req.query.slack_name;
+    const track = req.query.track;
+
+    const currentDate = new Date().toDateString();
+    const utcTime = new Date().toISOString();
+
+    const jsonResponse = {
+        slack_name: Poesitor,
+        current_day: currentDate,
+        utc_time: utcTime,
+        track: "backend",
+        //github_file_url:
+        //github_repo_url:
+        status_code: 200,
+    };
+
+    res.json(jsonResponse);
+})
+
